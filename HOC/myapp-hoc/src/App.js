cdfa4ced1a1c4d79bc8e041c196efed679c9fa1e
srraiyani259 <srraiyani259@gmail.com>
2024-05-06 18:30:13 +0530

    3-5-24

---
+++ HOC/myapp-hoc/src/App.js
@@ -0,0 +1,16 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Person1 from './Person1';
+import Person2 from './Person2';
+
+function App() {
+  return (
+   <>
+   <Person1 />
+   <Person2 />
+   </>
+  );
+}
+
+export default App;
