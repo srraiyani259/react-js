2d63a7e942efef17f3eb4b55f1ff3eb73eda5d01
srraiyani259 <srraiyani259@gmail.com>
2024-06-19 18:49:34 +0530

    30-5-24

---
+++ useReducer/myapp-red/src/App.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Example from './Example';
+
+function App() {
+  return (
+    <div className="App">
+       <Example/>
+    </div>
+  );
+}
+
+export default App;
