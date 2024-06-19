212369e5c370fae76b6cedb2fcdaaf705b60b01f
srraiyani259 <srraiyani259@gmail.com>
2024-06-19 18:39:37 +0530

    29-5-24

---
+++ useContextPR/myapp-contextpr/src/Header.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Header = ({ toggleTheme }) => {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </header>
+  );
+};
+
+export default Header;
