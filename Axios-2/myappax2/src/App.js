bc8931fa825dd796d53563d76f5482c02f67645c
srraiyani259 <srraiyani259@gmail.com>
2024-06-22 23:00:11 +0530

    19-6-24

---
+++ Axios-2/myappax2/src/App.js
@@ -0,0 +1,16 @@
+
+import logo from './logo.svg';
+import './App.css';
+import PostingData from './PostingData';
+
+
+function App() {
+  return (
+    <>
+    <PostingData />
+    </>
+    
+  );
+}
+
+export default App;
