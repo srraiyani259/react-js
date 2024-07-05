9fba922cfbc529c5f517c3581acb701dc7d51bc2
srraiyani259 <srraiyani259@gmail.com>
2024-07-05 23:16:19 +0530

    28-6-24

--- GoogleAuth/myappauth/src/Signin.jsx
+++ GoogleAuth/myappauth/src/Signin.jsx
@@ -1,58 +1,55 @@
-import React,{useState , useEffect} from 'react'
-import { signInWithPopup } from "firebase/auth";
-import { auth , provider } from './Config';
+import React, { useState, useEffect } from 'react';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { auth, provider } from './Config';
 import Home from './Home';
-import { signOut } from 'firebase/auth'
+import './App.css'; // Import the CSS file
 
 function Signin() {
+    const [value, setValue] = useState("");
 
-    const [value , setValue] = useState("")
+    const handleClick = () => {
+        signInWithPopup(auth, provider)
+            .then((data) => {
+                setValue(data.user.email);
+                localStorage.setItem("email", data.user.email);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-    const handleClick=()=>{
-        signInWithPopup(auth , provider)
-        .then((data)=>{
-            setValue(data.user.email)
-            localStorage.setItem("email",data.user.email)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }
-
-    const handleSignout=()=>{
+    const handleSignout = () => {
         signOut(auth)
-        .then(()=>{
-            localStorage.removeItem("email",)
-            setValue('')
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }
+            .then(() => {
+                localStorage.removeItem("email");
+                setValue('');
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-    useEffect(()=>{
-        
-        let userEmail = localStorage.getItem("email")
-        if(userEmail){
-            setValue(userEmail)
+    useEffect(() => {
+        let userEmail = localStorage.getItem("email");
+        if (userEmail) {
+            setValue(userEmail);
         }
-
-    },[])
+    }, []);
 
     return (
-        <div>
-          {value ? 
-          
-          <>
-          <h1>Welcome !{value.displayName}</h1>
-          <Home/>
-          <button onClick={handleSignout}>Sign Out</button>
-          <img src={value.photoURL}/>
-          </>
-          : <button onClick={handleClick}>SingIN With Google</button>}
+        <div className="container">
+            {value ? (
+                <>
+                    <h1>Welcome! {value}</h1>
+                    <Home />
+                    <button onClick={handleSignout}>Sign Out</button>
+                    <img src={value.photoURL} alt="Profile" />
+                </>
+            ) : (
+                <button onClick={handleClick}>Sign In With Google</button>
+            )}
         </div>
-      )
-
+    );
 }
 
-export default Signin
+export default Signin;
