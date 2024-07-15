1f3c2a332c6dedf1456cff5d0973fae3d69c6a76
srraiyani259 <srraiyani259@gmail.com>
2024-07-15 17:46:33 +0530

    10-7-24

---
+++ useCallback/myapp/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react';
+import Parent from './Parent';
+
+
+
+const App = () => {
+  return (
+    <div>
+      
+   <Parent/>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ useCallback/myapp/src/Child.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { memo } from 'react'
+import { useState } from 'react'
+
+function Child({handleinc , countwo}) {
+    console.log("child component")
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default memo(Child)
